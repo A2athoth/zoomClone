b6def7396809822f09ca7c62cf4152b82bc4a5f9
Sarc-Office <sarc@ntcon.co>
2021-12-28 15:59:14 +0900

    ~2.1

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -10,17 +10,14 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);      // 이렇게 세팅한것만으로 localhost:3000/socket.io/socket.io.js  접근이 가능
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+wsServer.on("connection", (socket) => {
+    console.log(socket);
+});
 
-function onSocketClose() {
-    console.log("Disconnected from the Browser ❌");
-}
-
-const sockets = [];
-
+/*const sockets = [];
 wss.on("connection", (socket) => {
     sockets.push(socket);
     socket["nickname"] = "Unknown";
@@ -41,6 +38,7 @@ wss.on("connection", (socket) => {
                 break;
         }
     });
-});
+});*/
 
-server.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
