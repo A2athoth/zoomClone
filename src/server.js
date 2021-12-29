90718c5911470ab65fb00bf67a6cf14190c0b0b4
Sarc-Office <sarc@ntcon.co>
2021-12-29 18:38:09 +0900

    ~2.7 이나 닉네임 관련 핸들링이 안되어있음

--- src/server.js
+++ src/server.js
@@ -14,45 +14,26 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);      // 이렇게 세팅한것만으로 localhost:3000/socket.io/socket.io.js  접근이 가능
 
 wsServer.on("connection", (socket) => {
+    socket["nickname"] = "Unknown";
     socket.onAny((event) => {
         console.log(`Socket Event: ${event}`);
     });
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
         done();
-        socket.to(roomName).emit("welcome");
+        socket.to(roomName).emit("welcome", socket.nickname);
     });
     socket.on("disconnecting", () => {
-        socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+        socket.rooms.forEach((room) =>
+            socket.to(room).emit("leave", socket.nickname)
+        );
     });
     socket.on("new_message", (msg, room, done) => {
         socket.to(room).emit("new_message", msg);
         done();
     });
+    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
-
-/*const sockets = [];
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Unknown";
-    console.log("Connected to Browser ✅");
-    socket.on("close", onSocketClose);
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type) {
-            case "new_message":
-                sockets.forEach((aSocket) =>
-                    aSocket.send(`${socket.nickname}: ${message.payload}`)
-                );
-                break;
-            case "nickname":
-                socket["nickname"] = message.payload;
-                break;
-            default:
-                break;
-        }
-    });
-});*/
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
