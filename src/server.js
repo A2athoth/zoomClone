3f9333c6ef8ed3f9d1d711d3f4408d4585f762f7
Sarc-Office <sarc@ntcon.co>
2021-12-30 12:49:47 +0900

    ~2.7 과 닉네임쪽 핸들링 되어있긴 함.(변경관련 내용은 안되있음)

--- src/server.js
+++ src/server.js
@@ -13,6 +13,21 @@ app.get("/*", (_, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);      // 이렇게 세팅한것만으로 localhost:3000/socket.io/socket.io.js  접근이 가능
 
+function publicRooms() {
+    const {
+        sockets: {
+            adapter: { sids, rooms },
+        },
+    } = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+}
+
 wsServer.on("connection", (socket) => {
     socket["nickname"] = "Unknown";
     socket.onAny((event) => {
@@ -22,17 +37,23 @@ wsServer.on("connection", (socket) => {
         socket.join(roomName);
         done();
         socket.to(roomName).emit("welcome", socket.nickname);
+        wsServer.sockets.emit("room_change", publicRooms());
     });
     socket.on("disconnecting", () => {
         socket.rooms.forEach((room) =>
             socket.to(room).emit("leave", socket.nickname)
         );
     });
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
+    });
     socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", msg);
+        socket.to(room).emit("new_message", `${socket.nickname} : ${msg}`);
         done();
     });
-    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+    socket.on("nickname", (nickname) => {
+        (socket["nickname"] = nickname)
+    });
 });
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
